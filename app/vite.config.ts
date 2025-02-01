7403ebfaa1916fd733fbde2b260c0b02a2392fca
Alexander Senger <senger.alexander@yahoo.de>
2025-02-01 16:11:07 +0000

    add vite config param type

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import symfonyPlugin from 'vite-plugin-symfony';
 import Components from 'unplugin-vue-components/vite';
@@ -41,124 +41,126 @@ const fontFamilies = [
   },
 ];
 
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  // const isProduction = mode === 'production';
-  // const isDevelopment = command === 'serve';
+export default defineConfig(
+  ({ command, mode, isSsrBuild, isPreview }: ConfigEnv) => {
+    // const isProduction = mode === 'production';
+    // const isDevelopment = command === 'serve';
 
-  return {
-    plugins: [
-      Vue(),
-      AutoImport({
-        dts: './assets/auto-imports.d.ts', // enable typescript support
-        imports: ['vue', '@vueuse/core'],
-      }),
-      Icons({ compiler: 'vue3', scale: 1 }),
-      Components({
-        dts: './assets/icons.d.ts', // enable typescript support (create on build)
-        version: 3,
-        resolvers: [
-          IconsResolver({
-            prefix: 'icon',
-          }),
-        ],
-      }),
-      Unfonts({
-        custom: {
-          families: fontFamilies,
+    return {
+      plugins: [
+        Vue(),
+        AutoImport({
+          dts: './assets/auto-imports.d.ts', // enable typescript support
+          imports: ['vue', '@vueuse/core'],
+        }),
+        Icons({ compiler: 'vue3', scale: 1 }),
+        Components({
+          dts: './assets/icons.d.ts', // enable typescript support (create on build)
+          version: 3,
+          resolvers: [
+            IconsResolver({
+              prefix: 'icon',
+            }),
+          ],
+        }),
+        Unfonts({
+          custom: {
+            families: fontFamilies,
 
+            /**
+             * Defines the default `font-display` value used for the generated
+             * `@font-rule` classes.
+             */
+            display: 'auto',
+
+            /**
+             * Using `<link rel="preload">` will trigger a request for the WebFont
+             * early in the critical rendering path, without having to wait for the
+             * CSSOM to be created.
+             */
+            preload: true,
+
+            /**
+             * Using `<link rel="prefetch">` is intended for prefetching resources
+             * that will be used in the next navigation/page load
+             * (e.g. when you go to the next page)
+             *
+             * Note: this can not be used with `preload`
+             */
+            prefetch: true,
+
+            /**
+             * define where the font load tags should be inserted
+             * default: 'head-prepend'
+             *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+             */
+            injectTo: 'head-prepend',
+          },
+        }),
+        symfonyPlugin({
           /**
-           * Defines the default `font-display` value used for the generated
-           * `@font-rule` classes.
+           * or define custom path for your controllers.json
+           * stimulus: './assets/other-dir/controllers.json
            */
-          display: 'auto',
+          stimulus: true,
 
-          /**
-           * Using `<link rel="preload">` will trigger a request for the WebFont
-           * early in the critical rendering path, without having to wait for the
-           * CSSOM to be created.
-           */
-          preload: true,
-
-          /**
-           * Using `<link rel="prefetch">` is intended for prefetching resources
-           * that will be used in the next navigation/page load
-           * (e.g. when you go to the next page)
-           *
-           * Note: this can not be used with `preload`
-           */
-          prefetch: true,
-
-          /**
-           * define where the font load tags should be inserted
-           * default: 'head-prepend'
-           *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
-           */
-          injectTo: 'head-prepend',
-        },
-      }),
-      symfonyPlugin({
-        /**
-         * or define custom path for your controllers.json
-         * stimulus: './assets/other-dir/controllers.json
-         */
-        stimulus: true,
-
-        // as we set `server.host` to 0.0.0.0
-        // we must explicitly set the server host name
-        viteDevServerHostname: 'localhost',
-      }),
-      tsconfigPaths(),
-    ],
-    publicDir: 'public',
-    base: '/build',
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler',
-        },
-      },
-    },
-    build: {
-      assetsInlineLimit: 512,
-      outDir: 'public/build',
-      manifest: true,
-      rollupOptions: {
-        input: {
-          login: './assets/styles/pages/login.scss',
-          main: './assets/vue/main.ts',
-
-          app: './assets/app.ts',
-          theme: './assets/styles/theme.scss',
-        },
-        output: {
-          manualChunks: {
-            vue: ['vue'],
+          // as we set `server.host` to 0.0.0.0
+          // we must explicitly set the server host name
+          viteDevServerHostname: 'localhost',
+        }),
+        tsconfigPaths(),
+      ],
+      publicDir: 'public',
+      base: '/build',
+      css: {
+        preprocessorOptions: {
+          scss: {
+            api: 'modern-compiler',
           },
         },
       },
-    },
-    server: {
-      // Required to listen on all interfaces
-      host: '0.0.0.0',
-      port: 3000,
-      hmr: {
-        host: '192.168.178.37', // localhost or your Docker host IP
-        protocol: 'ws',
+      build: {
+        assetsInlineLimit: 512,
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+          input: {
+            login: './assets/styles/pages/login.scss',
+            main: './assets/vue/main.ts',
+
+            app: './assets/app.ts',
+            theme: './assets/styles/theme.scss',
+          },
+          output: {
+            manualChunks: {
+              vue: ['vue'],
+            },
+          },
+        },
       },
-      fs: {
-        allow: ['.'],
+      server: {
+        // Required to listen on all interfaces
+        host: '0.0.0.0',
+        port: 3000,
+        hmr: {
+          host: '192.168.178.37', // localhost or your Docker host IP
+          protocol: 'ws',
+        },
+        fs: {
+          allow: ['.'],
+        },
+        watch: {
+          ignored: ['**/.idea/**', '**/tests/**', '**/var/**', '**/vendor/**'],
+        },
       },
-      watch: {
-        ignored: ['**/.idea/**', '**/tests/**', '**/var/**', '**/vendor/**'],
+      preview: {
+        host: '0.0.0.0',
+        port: 3005,
+        hmr: {
+          host: '192.168.178.37', // localhost or your Docker host IP
+          protocol: 'ws',
+        },
       },
-    },
-    preview: {
-      host: '0.0.0.0',
-      port: 3005,
-      hmr: {
-        host: '192.168.178.37', // localhost or your Docker host IP
-        protocol: 'ws',
-      },
-    },
-  };
-});
+    };
+  }
+);
