1e22ec0ad1079fa08f4e294db576a3c5bc5dfd53
Alexander Senger <senger.alexander@yahoo.de>
2025-07-27 16:04:07 +0000

    fix tooltip margins for bottom nav items

--- app/assets/vue/controllers/partials/sidebar/Tooltip.vue
+++ app/assets/vue/controllers/partials/sidebar/Tooltip.vue
@@ -5,6 +5,10 @@ defineProps({
     type: String,
     required: true
   },
+  marginLeft: {
+    type: String,
+    default: 'ml-6'
+  },
 })
 
 import { useSidebarStore } from '@/vue/stores/sidebar'
@@ -13,8 +17,8 @@ const sidebarStore = useSidebarStore();
 </script>
 
 <template>
-  <div v-if="!sidebarStore.isExpanded"
-    class="ml-6 rounded-md absolute top-[50%] -translate-y-[50%] whitespace-nowrap px-2 py-1 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover/navitem:visible group-hover/navitem:opacity-100 group-hover/navitem:translate-x-0 group-focus-within/navitem:visible group-focus-within/navitem:opacity-100 group-focus-within/navitem:translate-x-0">
+  <div v-if="!sidebarStore.isExpanded" :class="marginLeft"
+    class="rounded-md absolute top-[50%] -translate-y-[50%] whitespace-nowrap px-2 py-1 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover/navitem:visible group-hover/navitem:opacity-100 group-hover/navitem:translate-x-0 group-focus-within/navitem:visible group-focus-within/navitem:opacity-100 group-focus-within/navitem:translate-x-0">
     {{ text }}
   </div>
 </template>
