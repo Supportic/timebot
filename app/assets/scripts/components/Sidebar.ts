8a274ba3c42c45758b59021504d2eb3eb76f8a02
Alexander Senger <senger.alexander@yahoo.de>
2025-10-18 20:53:14 +0000

    move settings classes in sidebar to css file with sidebar state

--- app/assets/scripts/components/Sidebar.ts
+++ app/assets/scripts/components/Sidebar.ts
@@ -2,25 +2,14 @@ import SessionUpdater from '@/scripts/utils/SessionUpdater';
 
 export default class Sidebar {
   private readonly BREAKPOINT_MD: number = 768;
-  private readonly SIDEBAR_OPEN_CLASS: string = 'open';
+  private readonly SIDEBAR_STATE_EXPANDED: string = 'expanded';
+  private readonly SIDEBAR_STATE_MINIMIZED: string = 'minimized';
 
   private SidebarId: string = 'sidebar';
   private SidebarToggleButtonId: string = 'sidebar-toggle';
-  private SidebarToggleIconOpenedId: string = 'sidebar-toggle-icon-opened';
-  private SidebarToggleIconClosedId: string = 'sidebar-toggle-icon-closed';
-  private AppNameId: string = 'sidebar-app-name';
-  private AppProfileId: string = 'sidebar-user-profile';
-  private NavLinksSelector: string = '.nav-link span';
-  private TooltipsSelector: string = '.sidebar-tooltip';
 
   private Sidebar!: HTMLDivElement;
   private SidebarToggleButton!: HTMLButtonElement;
-  private SidebarToggleIconOpened!: HTMLElement;
-  private SidebarToggleIconClosed!: HTMLElement;
-  private AppName!: HTMLSpanElement;
-  private AppProfile!: HTMLDivElement;
-  private NavLinks: HTMLSpanElement[] = [];
-  private Tooltips: HTMLDivElement[] = [];
 
   constructor() {
     const sidebar = document.getElementById(
@@ -41,78 +30,22 @@ export default class Sidebar {
     this.Sidebar = sidebar;
     this.SidebarToggleButton = sidebarToggleButton;
 
-    this.SidebarToggleIconOpened = document.getElementById(
-      this.SidebarToggleIconOpenedId,
-    ) as HTMLElement;
-    this.SidebarToggleIconClosed = document.getElementById(
-      this.SidebarToggleIconClosedId,
-    ) as HTMLElement;
-    this.AppName = document.getElementById(this.AppNameId) as HTMLSpanElement;
-    this.AppProfile = document.getElementById(
-      this.AppProfileId,
-    ) as HTMLDivElement;
-    this.NavLinks = [
-      ...document.querySelectorAll<HTMLSpanElement>(this.NavLinksSelector),
-    ];
-    this.Tooltips = [
-      ...document.querySelectorAll<HTMLDivElement>(this.TooltipsSelector),
-    ];
-
     this.initializeResizeObserver();
     this.SidebarToggleButton.addEventListener('click', this.toggleSidebar);
   }
 
   public isExpanded = (): boolean => {
-    return this.Sidebar.classList.contains(this.SIDEBAR_OPEN_CLASS);
+    return this.Sidebar.dataset.state === this.SIDEBAR_STATE_EXPANDED;
   };
 
   expand = () => {
-    this.Sidebar.classList.add(this.SIDEBAR_OPEN_CLASS);
-
-    this.SidebarToggleIconOpened.classList.remove('hidden');
-    this.SidebarToggleIconClosed.classList.add('hidden');
-
-    this.AppName.classList.replace('w-0', 'w-32');
-    this.AppName.nextElementSibling?.classList.remove('mr-auto');
-
-    this.AppProfile.classList.add('w-52', 'ml-3');
-    this.AppProfile.classList.remove('w-0');
-
-    this.NavLinks.forEach((navLink: HTMLSpanElement) => {
-      navLink.classList.add('w-52', 'ml-3');
-      navLink.classList.remove('w-0');
-    });
-
-    this.Tooltips.forEach((tooltip: HTMLDivElement) => {
-      tooltip.classList.add('hidden');
-    });
+    this.Sidebar.dataset.state = this.SIDEBAR_STATE_EXPANDED;
 
     SessionUpdater.update('sidebar_expanded', true);
   };
 
   minimize = () => {
-    this.Sidebar.classList.remove(this.SIDEBAR_OPEN_CLASS);
-
-    this.SidebarToggleIconOpened.classList.add('hidden');
-    this.SidebarToggleIconClosed.classList.remove('hidden');
-
-    this.AppName.classList.replace('w-32', 'w-0');
-    this.AppName.nextElementSibling?.classList.add('mr-auto');
-
-    this.AppProfile.classList.add('w-52', 'ml-3');
-    this.AppProfile.classList.remove('w-0');
-
-    this.AppProfile.classList.add('w-0');
-    this.AppProfile.classList.remove('w-52', 'ml-3');
-
-    this.NavLinks.forEach((navLink: HTMLSpanElement) => {
-      navLink.classList.add('w-0');
-      navLink.classList.remove('w-52', 'ml-3');
-    });
-
-    this.Tooltips.forEach((tooltip: HTMLDivElement) => {
-      tooltip.classList.remove('hidden');
-    });
+    this.Sidebar.dataset.state = this.SIDEBAR_STATE_MINIMIZED;
 
     SessionUpdater.update('sidebar_expanded', false);
   };
